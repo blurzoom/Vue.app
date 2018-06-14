dabbf8763b72e44c8764387714701dae20f3396a
Oleksii Herasymenko <blurzoom@gmail.com>
2018-06-14 20:39:27 +0300

    Add logic registration on Firebase

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,54 +1,55 @@
 <template>
-    <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-            <v-flex xs12 sm8 md6>
-                <v-card class="elevation-12">
-                    <v-toolbar dark color="primary">
-                        <v-toolbar-title>Login form</v-toolbar-title>
-                    </v-toolbar>
-                    <v-card-text>
-                        <v-form ref="form" v-model="valid" lazy-validation>
-                            <v-text-field
-                                    prepend-icon="person"
-                                    name="email"
-                                    label="Email"
-                                    type="email"
-                                    v-model="email"
-                                    :rules="emailRules"
-                            ></v-text-field>
-                            <v-text-field
-                                    id="password"
-                                    prepend-icon="lock"
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    :counter="6"
-                                    v-model="password"
-                                    :rules="passwordRules"></v-text-field>
-                            <v-text-field
-                                    id="confirmPassword"
-                                    prepend-icon="lock"
-                                    name="confirmPassword"
-                                    label="Confirm Password"
-                                    type="password"
-                                    :counter="6"
-                                    v-model="confirmPassword"
-                                    :rules="confirmPasswordRules"></v-text-field>
-                        </v-form>
-                    </v-card-text>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn
-                                color="primary"
-                                @click="onSubmit"
-                                :disabled="!valid"
-                        >Login
-                        </v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Login form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                prepend-icon="person"
+                name="email"
+                label="Email"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field
+                id="password"
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+                :counter="6"
+                v-model="password"
+                :rules="passwordRules"></v-text-field>
+              <v-text-field
+                id="confirmPassword"
+                prepend-icon="lock"
+                name="confirmPassword"
+                label="Confirm Password"
+                type="password"
+                :counter="6"
+                v-model="confirmPassword"
+                :rules="confirmPasswordRules"></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              @click="onSubmit"
+              :loading="loading"
+              :disabled="!valid || loading"
+            >Login
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -74,6 +75,11 @@
         ]
       }
     },
+    computed: {
+      loading () {
+        return this.$store.getters.loading
+      }
+    },
     methods: {
       onSubmit () {
         if (this.$refs.form.validate()) {
@@ -81,7 +87,13 @@
             email: this.email,
             password: this.password
           }
-          alert('Ви вказали емайл: ' + user.email + ', пароль: ' + user.password)
+          this.$store.dispatch('registerUser', user)
+            .then(() => {
+              this.$router.push('/')
+            })
+            .catch(err => console.log(err))
+
+          // alert('Ви вказали емайл: ' + user.email + ', пароль: ' + user.password)
         }
       }
     }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import store from './store'
+import Vuetify from 'vuetify'
+import * as fb from 'firebase'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
@@ -15,5 +16,15 @@ new Vue({
   router,
   store,
   components: {App},
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyD2ZWW8_hdp-bW5xfu4aVfuvIY8ny5Ak7Q',
+      authDomain: 'it-ads-ad59a.firebaseapp.com',
+      databaseURL: 'https://it-ads-ad59a.firebaseio.com',
+      projectId: 'it-ads-ad59a',
+      storageBucket: 'it-ads-ad59a.appspot.com',
+      messagingSenderId: '1094875076714'
+    })
+  }
 })
